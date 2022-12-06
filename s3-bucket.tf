3db7f21a917f4f5732ac2251eef73d589a54a64f
rizqiarip <rizqiarifwibowo19@gmail.com>
2022-12-06 07:17:35 +0000

    add s3-bucket

---
+++ s3-bucket.tf
@@ -0,0 +1,12 @@
+module "s3_bucket" {
+  source = "app.terraform.io/rizqiarip/s3-bucket/aws"
+  version = "2.8.0"
+
+  bucket_prefix = var.prefix
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
+}
